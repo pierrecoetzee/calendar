e566ac967b05bad0aa85cca549f4b33061f10883
pierrecoetzee <pierre.coetzee@gmail.com>
2014-08-23 16:35:46 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appearhere::Application.config.secret_token = 'e210fb362a975ee6e50557cf76100f6e25c24a3ea7958f6b586f3a7829b00e20c62892ea1fc7a6279b7fc5ad435fd0975709e77cee6ee55c39ad3294c356969a'
